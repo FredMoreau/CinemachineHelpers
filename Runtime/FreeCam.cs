520d74d73ba125fbdc0ecd0c3925bf4969f69bcc
Fred Moreau <50830458+FredMoreau@users.noreply.github.com>
2019-09-22 17:33:25 +0200

    Added multi speed motion "a la Unreal"

--- Runtime/FreeCam.cs
+++ Runtime/FreeCam.cs
@@ -19,8 +19,13 @@ namespace Cinemachine.Helpers
 		[SerializeField] private string _horizontalMotionInput = "Horizontal";
 		[SerializeField] private string _forwardMotionInput = "Vertical";
 		[SerializeField] private string _upwardMotionInput = "UpDown";
-		[SerializeField] private float _speed = 1f;
+		[SerializeField] private KeyCode _fastKeycode = KeyCode.LeftShift;
+		[SerializeField] private KeyCode _fasterKeycode = KeyCode.LeftControl;
+		[SerializeField] private float _normalSpeed = 1f;
+		[SerializeField] private float _fastSpeed = 5f;
+		[SerializeField] private float _fasterSpeed = 20f;
 
+		private float _speed;
 		Vector3 _position;
 
 		private CinemachineBrain vBrain;
@@ -36,7 +41,7 @@ namespace Cinemachine.Helpers
 		{
 			if ((CinemachineVirtualCamera)vBrain.ActiveVirtualCamera != vCam)
 				return;
-
+			_speed = Input.GetKey(_fasterKeycode) ? _fasterSpeed : Input.GetKey(_fastKeycode) ? _fastSpeed : _normalSpeed;
 			_position.x = Input.GetAxis(_horizontalMotionInput) * Time.deltaTime * _speed;
 			_position.z = Input.GetAxis(_forwardMotionInput) * Time.deltaTime * _speed;
 			_position.y = Input.GetAxis(_upwardMotionInput) * Time.deltaTime * _speed;
